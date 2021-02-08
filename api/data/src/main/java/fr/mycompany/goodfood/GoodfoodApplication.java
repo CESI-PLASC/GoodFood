3ac50aa42941cdac3f8a05fc755262721ab86dd6
Sylvain Decombe <sylvain.decombe@viacesi.fr>
2021-02-08 14:31:31 +0100

    Swagger Spring

--- api/data/src/main/java/fr/mycompany/goodfood/GoodfoodApplication.java
+++ api/data/src/main/java/fr/mycompany/goodfood/GoodfoodApplication.java
@@ -3,9 +3,11 @@ package fr.mycompany.goodfood;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @RestController
+@EnableSwagger2
 public class GoodfoodApplication {
 
 	public static void main(String[] args) {
