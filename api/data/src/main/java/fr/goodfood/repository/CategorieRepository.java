b49d2c0125e431d95c1c76b6a9972d6de0c4c2fc
cgiboulot <clement.giboulot@erudo.fr>
2021-02-12 10:40:50 +0100

    FIX Error List/Set Commande

--- api/data/src/main/java/fr/goodfood/repository/CategorieRepository.java
+++ api/data/src/main/java/fr/goodfood/repository/CategorieRepository.java
@@ -5,6 +5,8 @@ import fr.goodfood.entity.Franchise;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategorieRepository extends JpaRepository<Categorie, Long> {
 
