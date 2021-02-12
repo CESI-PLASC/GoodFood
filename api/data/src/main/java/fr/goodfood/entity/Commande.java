f85e0831dd81701c47ad6479ac6d8f65e4a40e99
Neil3ru4 <gillereauaurelien@gmail.com>
2021-02-12 14:44:37 +0100

    Api data: Modification de la stratégie de génération des identifiants d'entités

--- api/data/src/main/java/fr/goodfood/entity/Commande.java
+++ api/data/src/main/java/fr/goodfood/entity/Commande.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -22,7 +23,7 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 public class Commande {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
