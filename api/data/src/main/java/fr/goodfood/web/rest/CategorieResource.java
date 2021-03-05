494142b8c45dd8ade131ca982e7e470e3b7df973
Neil3ru4 <gillereauaurelien@gmail.com>
2021-03-05 18:11:36 +0100

    Ressource pour les catégories, ajout des dtos

--- api/data/src/main/java/fr/goodfood/web/rest/CategorieResource.java
+++ api/data/src/main/java/fr/goodfood/web/rest/CategorieResource.java
@@ -1,7 +1,11 @@
 package fr.goodfood.web.rest;
 
+import fr.goodfood.dto.categorie.CategorieDTO;
+import fr.goodfood.dto.categorie.SimpleCategorieDTO;
 import fr.goodfood.entity.Categorie;
 import fr.goodfood.service.CategorieService;
+import fr.goodfood.service.mapper.CategorieMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -23,18 +29,21 @@ public class CategorieResource {
     @Autowired
     private CategorieService categorieService;
 
+    @Inject
+    CategorieMapper categorieMapper;
+
     @GetMapping
-    public CollectionModel<Categorie> all() {
+    public CollectionModel<CategorieDTO> all() {
         List<Categorie> categories = this.categorieService.all();
 
-        return CollectionModel.of(categories, linkTo(methodOn(CategorieResource.class).all()).withSelfRel());
+        return CollectionModel.of(this.categorieMapper.toDtos(categories), linkTo(methodOn(CategorieResource.class).all()).withSelfRel());
     }
 
     @PostMapping
-    public EntityModel<Categorie> create(@RequestBody Categorie categorie) {
+    public EntityModel<CategorieDTO> create(@RequestBody Categorie categorie) {
         Categorie nouvelleCategorie = this.categorieService.create(categorie);
 
-        return EntityModel.of(nouvelleCategorie,
+        return EntityModel.of(this.categorieMapper.toDto(nouvelleCategorie),
                 linkTo(methodOn(CategorieResource.class).create(categorie)).withSelfRel());
     }
 }
