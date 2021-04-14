d1a5238d05a7c7fa24017a50d4c0f08b5f7df7e8
cgiboulot <clement.giboulot@erudo.fr>
2021-04-14 14:59:51 +0200

    Correction des imports du merge

--- app/good-food/src/app/shared/constants/icons.constant.ts
+++ app/good-food/src/app/shared/constants/icons.constant.ts
@@ -1,4 +1,6 @@
-import { faCheckSquare, faChevronDown, faChevronRight, faMinus, faPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
+import {
+    faCheckSquare, faChevronDown, faChevronRight, faEye, faMinus, faPlus, faSearch, faSyncAlt, faTimes
+} from '@fortawesome/free-solid-svg-icons';
 import { faSquare } from '@fortawesome/free-regular-svg-icons';
 
 
