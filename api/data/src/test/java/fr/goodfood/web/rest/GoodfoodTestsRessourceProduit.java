740d19dd51baa154f3c951dd3a062dfa66edfdf9
cgiboulot <clement.giboulot@erudo.fr>
2021-04-16 10:42:06 +0200

    Tests java pour Sylvain

---
+++ api/data/src/test/java/fr/goodfood/web/rest/GoodfoodTestsRessourceProduit.java
@@ -0,0 +1,34 @@
+package fr.goodfood.web.rest;
+
+import fr.goodfood.GoodfoodTestsConfiguration;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+@GoodfoodTestsConfiguration
+public class GoodfoodTestsRessourceProduit {
+
+    @Test
+    void testRetourneLeProduitCree() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testMetAJourLeProduit() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testRetourneLeProduitIdUn() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testRetourneToutsLesProduits() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testSupprimeLeProduit() {
+        fail("Not yet implemented");
+    }
+}
