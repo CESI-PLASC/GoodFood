7e84e65f58d317c0f0e25804f686693f5f8c1028
Sylvain Decombe <sylvain.decombe@viacesi.fr>
2021-05-20 09:38:49 +0200

    Run de l'app + Rectification datas

--- projet/migration/sql/goodfood-data.sql
+++ projet/migration/sql/goodfood-data.sql
@@ -82,8 +82,7 @@ insert into goodfood.localisation (utilisateur_id, adresse_id, preferee) values
     (5, 1, 0),
     (5, 3, 0),
     (5, 4, 0),
-    (5, 5, 0),
-    (5, 2, 1)
+    (5, 5, 0)
 ;
 
 insert into goodfood.propose (formule_id, franchise_id, prix) values
