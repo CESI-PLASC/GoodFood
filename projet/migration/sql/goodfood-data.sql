0e2b11be2f707aa8eaa96a451f2a7a3a9edd05cd
GILLEREAU Aurélien <gillereauaurelien@gmail.com>
2021-06-14 10:51:22 +0200

    Fix du script de mise à jour de l'api dotnet. Fix du seeding de données. Fix de dotnet watch

--- projet/migration/sql/goodfood-data.sql
+++ projet/migration/sql/goodfood-data.sql
@@ -1,22 +1,22 @@
-insert into goodfood.Categorie (id, designation) values
+insert into goodfood.categorie (id, designation) values
 	(1, "Entrée"),
     (2, "Plat"),
     (3, "Déssert")
 ;
 
-insert into goodfood.Statut (id, designation) values
+insert into goodfood.statut (id, designation) values
 	(1, "En attente"),
     (2, "En cours"),
     (3, "En livraison"),
     (4, "Délivré")
 ;
 
-insert into goodfood.Formule (id, designation, prix) values
+insert into goodfood.formule (id, designation, prix) values
 	(1, "Petit creux", 14.99),
     (2, "Gourmande", 24.99)
 ;
 
-insert into goodfood.Adresse (id, pays, code_postal, rue, ville) values
+insert into goodfood.adresse (id, pays, code_postal, rue, ville) values
 	(1, "France", "16200", "17 Route de Bellevue", "Jarnac"),
     (2, "France", "16100", "67Bis Rue François Premier", "Cognac"),
     (3, "France", "17100", "12 Route de l'insolence", "Saintes"),
@@ -32,19 +32,19 @@ insert into goodfood.utilisateur (id, nom, prenom, date_naissance, email, teleph
     (5, "Gaudu", "David", "1996-10-10 12:00:00", "gaudu.david@gmail.com", null, null, "cus_JILfKfybM9nRXM")
 ;
 
-insert into goodfood.Franchise (id, designation, email, telephone_fixe, telephone_mobile, adresse_id) values
+insert into goodfood.franchise (id, designation, email, telephone_fixe, telephone_mobile, adresse_id) values
 	(1, "Le premier franchisé", "franchise@test.fr", "0545050505", "0606060606", 1),
     (2, "Le second franchisé", "franchise2@test.fr", null, null, 2)
 ;
 
-insert into goodfood.Produit (id, designation, prix) values 
+insert into goodfood.produit (id, designation, prix) values 
 	(1, "Hamburger maison", 19.99),
     (2, "Salade césar", 14.99),
     (3, "Salade de fruits", 4.99),
     (4, "Pizza", 11.99)
 ;
 
-insert into goodfood.Commande (id, date_creation, date_donnee, statut_id, franchise_id, utilisateur_id) values 
+insert into goodfood.commande (id, date_creation, date_donnee, statut_id, franchise_id, utilisateur_id) values 
 	(1, "2020-01-01 12:00:00", "2020-01-01 12:00:00", 1, 1, 5),
     (2, "2020-01-01 12:00:00", "2020-01-01 12:00:00", 4, 1, 2)
 ;
@@ -82,8 +82,7 @@ insert into goodfood.localisation (utilisateur_id, adresse_id, preferee) values
     (5, 1, 0),
     (5, 3, 0),
     (5, 4, 0),
-    (5, 5, 0),
-    (5, 2, 1)
+    (5, 5, 0)
 ;
 
 insert into goodfood.propose (formule_id, franchise_id, prix) values
