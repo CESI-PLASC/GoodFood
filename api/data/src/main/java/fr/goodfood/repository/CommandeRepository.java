1719acb5e233101461b569ef2a47db164a905203
GILLEREAU Aurélien <gillereauaurelien@gmail.com>
2021-06-15 10:11:45 +0200

    Fix du calcul du prix d'une commande

--- api/data/src/main/java/fr/goodfood/repository/CommandeRepository.java
+++ api/data/src/main/java/fr/goodfood/repository/CommandeRepository.java
@@ -9,6 +9,6 @@ import fr.goodfood.entity.Commande;
 @Repository
 public interface CommandeRepository extends JpaRepository<Commande, Long> {
 
-    @Query(value = "select sum(prix) from goodfood.contenu inner join goodfood.formule f on f.id where commande_id = 1", nativeQuery = true)
+    @Query(value = "select sum(prix) from goodfood.contenu c inner join goodfood.formule f on f.id  = c.formule_id where c.commande_id = :idCommande", nativeQuery = true)
     Double getPrice(Long idCommande);
 }
