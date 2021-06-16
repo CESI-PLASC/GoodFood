3d2b24e69740969191903b240c2ff965f59bfdbd
GILLEREAU Aurélien <gillereauaurelien@gmail.com>
2021-06-16 11:09:24 +0200

    Modification de contient

--- api/data/src/main/java/fr/goodfood/web/rest/ContientResource.java
+++ api/data/src/main/java/fr/goodfood/web/rest/ContientResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,6 +59,14 @@ public class ContientResource {
         ContientDTO contientDto = this.contientMapper.toDto(contient);
 
         return EntityModel.of(contientDto, linkTo(methodOn(ContientResource.class).create(contient)).withSelfRel());
+    }
+
+    @PutMapping
+    public EntityModel<ContientDTO> update(@RequestBody Contient contientJson) {
+        Contient contient = this.contientService.update(contientJson);
+        ContientDTO contientDto = this.contientMapper.toDto(contient);
+
+        return EntityModel.of(contientDto, linkTo(methodOn(ContientResource.class).update(contientJson)).withSelfRel());
     }
 
     @DeleteMapping("/{id}")
