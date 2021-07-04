46c6fb77f2a36cb2741dc55c7ff7699100241d2e
Pierre Louis NAVAUD <pierrelouis@navaud.fr>
2021-07-04 17:47:07 +0200

    delete console log

--- api/data/src/main/java/fr/goodfood/service/FormuleService.java
+++ api/data/src/main/java/fr/goodfood/service/FormuleService.java
@@ -15,7 +15,6 @@ public class FormuleService {
     private FormuleRepository formuleRepository;
 
     public Formule create(Formule formule){
-        System.out.println(formule.getFranchises());
         return this.formuleRepository.save(formule);
     }
 
