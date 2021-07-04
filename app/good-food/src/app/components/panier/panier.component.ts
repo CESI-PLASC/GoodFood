c860d1899f6767e58858b453446a6573afd8d888
GILLEREAU Aurélien <gillereauaurelien@gmail.com>
2021-07-04 18:27:28 +0200

    Fix de la redirection du bouton "Modifier panier" de la page de validation d'une commande

--- app/good-food/src/app/components/panier/panier.component.ts
+++ app/good-food/src/app/components/panier/panier.component.ts
@@ -5,14 +5,16 @@ import { Commande } from 'src/app/shared/models/commande/commande';
 @Component({
   selector: 'gf-panier',
   templateUrl: './panier.component.html',
-  styleUrls: ['./panier.component.scss']
+  styleUrls: ['./panier.component.scss'],
 })
 export class PanierComponent {
   @Input() commande: Commande;
 
   constructor(private router: Router) {}
 
-  async navigateToCommande() : Promise<void> {
-    await this.router.navigate([`/franchise/${this.commande.franchise.id}/catalogue`]);
+  async navigateToCommande(): Promise<void> {
+    if (this.commande.id) {
+      await this.router.navigate([`/commande/${this.commande.id}`]);
+    }
   }
 }
